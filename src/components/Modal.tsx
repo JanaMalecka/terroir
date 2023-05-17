08fa1ef4d9bde9a3d3413b34d630d69062a9b452
JanaMalecka <jm.jmalecka@gmail.com>
2023-05-17 18:37:37 +0200

    modal

---
+++ src/components/Modal.tsx
@@ -0,0 +1,128 @@
+const Modal = (): JSX.Element => {
+  return (
+    <div className="modal-body">
+      <h1>TERROIR</h1>
+      <h2>ČEJKOVICE</h2>
+      <p>
+        Označení Terroir Čejkovice je možné přiznávat pouze vínům vyrobených z
+        odrůd:
+      </p>
+      <p> Rulandské modré a Tramín.</p>
+      <ul>
+        <li className="mb-3">
+          {' '}
+          Vína mohou být vyráběna pouze členy sdružení Čtvrtečníci - sdružení
+          vinařů Čejkovice, z.s.
+        </li>
+        <li className="mb-3">
+          Vína musí být vyráběna pouze z hroznů vypěstovaných na registrovaných
+          vinicích v katastru obce Čejkovice s minimálním počtem 4.500 keřů na
+          hektar. Hrozny musí být zdravé. Víno by mělo být ve větší míře
+          formované vinohradem = terroir. Výnos u odrůdy Rulandské modré nesmí
+          překročit 1,5 kg hroznů na keř a výnos u odrůdy Tramín nesmí překročit
+          2 kg hroznů na keř.
+        </li>
+        <li className="mb-3">
+          Hrozny musí být zpracovány bez exogenních enzymů a jiných maceračních
+          přídavných látek. Důležitá je minimalizace síření při výrobě vína. U
+          odrůdy Rulandské modré je povolena maximální hranice do 100 mg/l
+          veškerého SO 2 . U odrůdy Tramín je povolena maximální hranice do 120
+          mg/l veškerého SO 2. Není povolen přídavek kyseliny askorbové.
+        </li>
+        <li>
+          Vína lze vyrábět pouze z hroznů révy vinné odpovídajících
+          kvalitativnímu zařazení: víno s přívlastkem pozdní sběr a víno s
+          přívlastkem výběr z hroznů. Obsah alkoholu ve vínech musí být
+          minimálně 12,5 % obj.. Vína by měla být plná a extraktivní a musí
+          vykazovat typické senzorické vlastnosti dané odrůdy.
+        </li>
+        <li className="mb-3">
+          Vína musí být plněna do skleněných lahví o obsahu 0,75 l nebo 1,5 l.
+          Do degustační komise mohou být přihlášena pouze nalahvovaná vína.
+          Prodej vín je povolen od 1. 5. každého kalendářního roku při splnění
+          minimálních dob zrání vín. Po schválení vína degustační komisí budou
+          láhve označovány kolkem Terroir Čejkovice nalepeným kolem hrdla lahve.
+        </li>
+        <li className="mb-3">
+          Degustace probíhá minimálně jednou ročně. Degustační komise je složena
+          ze členů spolku Čtvrtečníci - sdružení vinařů Čejkovice, z.s. a
+          pracuje v minimálním počtu 11 osob.
+        </li>
+        <li className="mb-3">
+          Degustační komise hodnotí vína body a také o vínech hlasuje. Víno je
+          hodnoceno stobodovým systémem mezinárodní unie enologů, ale hodnotí se
+          pouze body „po pěti“ např.: 85, 90, 95 a 100. Minimální bodové
+          ohodnocení pro zařazení do Terroir Čejkovice je 85 bodů. Pokud je vínu
+          uděleno méně jak 80 bodů musí být napsáno odůvodnění. Dále je o víně
+          hlasováno, přičemž pro zařazení do Terroir Čejkovice je nutný souhlas
+          2/3 hodnotících členů degustační komise.
+        </li>
+      </ul>
+      <h4>
+        Senzorické vlastnosti a technologické požadavky pro jednotlivé odrůdy:
+      </h4>{' '}
+      <h5>RULANDSKÉ MODRÉ </h5>
+      <h6>
+        Typické rulandové, odrůdové, komplexní s jemným projevem po zrání v
+        sudu.
+      </h6>{' '}
+      <p>
+        - víno musí mít obsah zbytkového cukru (fruktóza + glukóza) maximálně 4
+        g/l
+      </p>
+      <p>- víno musí mít obsah alkoholu minimálně 12,5 % obj.</p>
+      <p>- víno musí zrát minimálně 12 měsíců v sudech</p>
+      <p>- musí být provedena jablečno-mléčná fermentace</p>
+      <p>
+        - víno musí zrát na jemných kalech minimálně 120 dní od konce fermentace
+        nebo 150 dní od sklizně
+      </p>
+      <p>
+        - prodej vín je možný nejdříve po 19 měsících od 1. října roku sklizně
+        hroznů
+      </p>
+      <p>
+        - při využití nových vypalovaných sudů pro maximálně 33 % celkového
+        objemu vína, je uvedení vín na trh pod označením Terroir Čejkovice možné
+        nejdříve po 19 měsících od 1. října roku sklizně hroznů
+      </p>
+      <p>
+        - při využití nových vypalovaných sudů pro více než 33 % celkového
+        objemu vína, je uvedení vín na trh pod označením Terroir Čejkovice možné
+        nejdříve po 31 měsících od 1. října roku sklizně hroznů{' '}
+      </p>
+      <h5>TRAMÍN</h5>
+      <h6>Hutné, harmonické a kořenité víno</h6>
+      <p>
+        - víno musí mít obsah zbytkového cukru (fruktóza + glukóza) maximálně 4
+        g/l
+      </p>
+      <p>- víno musí mít obsah alkoholu minimálně 12,5 % obj.</p>
+      <p>- víno musí zrát minimálně 9 měsíců</p>
+      <p>- hrozny musí být macerovány minimálně 24 hodin</p>
+      <p>- není povolena výroba technologií „oranžových vín“</p>
+      <p>
+        - víno musí zrát na jemných kalech minimálně 120 dní od konce fermentace
+        nebo 150 dní od sklizně
+      </p>{' '}
+      <p>
+        - prodej vín je možný nejdříve po 19 měsících od 1. října roku sklizně
+        hroznů
+      </p>
+      - při využití nových vypalovaných sudů pro maximálně 33 % celkového objemu
+      vína, je uvedení vín na trh pod označením Terroir Čejkovice možné nejdříve
+      po 19 měsících od 1. října roku sklizně hroznů
+      <p>
+        - při využití nových vypalovaných sudů pro více než 33 % celkového
+        objemu vína, je uvedení vín na trh pod označením Terroir Čejkovice možné
+        nejdříve po 31 měsících od 1. října roku sklizně hroznů
+      </p>
+      <h6> Závěrečné ustanovení </h6> Autor statutu Terroir Čejkovice -
+      Čtvrtečníci - sdružení vinařů Čejkovice, z.s. si vyhrazuje právo, jako
+      jediný subjekt, upravovat tento statut a to po projednání v pracovní
+      skupině a následném odsouhlasení členskou schůzí spolku.
+    </div>
+  );
+};
+
+export default Modal;
--- src/pages/Statute.tsx
+++ src/pages/Statute.tsx
@@ -1,9 +1,73 @@
 import React, { FC } from 'react';
+import Modal from '../components/Modal';
 
 const Statute: FC = (): JSX.Element => {
   return (
     <>
-      <div className="container__content">Statut</div>
+      <div className="container__content">
+        <h1>TERROIR</h1>
+        <h2>ČEJKOVICE</h2>
+        <p>
+          Označení Terroir Čejkovice je možné přiznávat pouze vínům vyrobených z
+          odrůd:
+        </p>
+        <p> Rulandské modré a Tramín.</p>
+        <ul>
+          <li className="mb-3">
+            {' '}
+            Vína mohou být vyráběna pouze členy sdružení Čtvrtečníci - sdružení
+            vinařů Čejkovice, z.s.
+          </li>
+          <li className="mb-3">
+            Vína musí být vyráběna pouze z hroznů vypěstovaných na
+            registrovaných vinicích v katastru obce Čejkovice s minimálním
+            počtem 4.500 keřů na hektar. Hrozny musí být zdravé. Víno by mělo
+            být ve větší míře formované vinohradem = terroir. Výnos u odrůdy
+            Rulandské modré nesmí překročit 1,5 kg hroznů na keř a výnos u
+            odrůdy Tramín nesmí překročit 2 kg hroznů na keř.
+          </li>
+        </ul>
+        <div>
+          <button
+            type="button"
+            className="btn--primary"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
+            Více o terroir
+          </button>
+          <div
+            className="modal fade"
+            id="exampleModal"
+            /*  tabindex="-1" */
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog modal-dialog-scrollable">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <Modal />
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn--secondary"
+                    data-bs-dismiss="modal"
+                  >
+                    Zavřít
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
