d19a82bce26c17e2e37636489e47aaedd620b872
JanaMalecka <jm.jmalecka@gmail.com>
2023-07-18 13:13:15 +0200

    Modal changed

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,59 @@
 const Modal = (): JSX.Element => {
   return (
     <div className="modal-body">
+      <p>
+        Označení Terroir Čejkovice je možné přiznávat pouze vínům vyrobených z
+        odrůd:
+      </p>
+      <p className="fw-bold"> Rulandské modré a Tramín.</p>
+      <p className="mb-3">
+        {' '}
+        Vína mohou být vyráběna pouze členy sdružení Čtvrtečníci - sdružení
+        vinařů Čejkovice, z.s.
+      </p>
+      <p className="mb-3">
+        Vína musí být vyráběna pouze z hroznů vypěstovaných na registrovaných
+        vinicích v katastru obce Čejkovice s minimálním počtem 4.500 keřů na
+        hektar. Hrozny musí být zdravé. Víno by mělo být ve větší míře formované
+        vinohradem = terroir. Výnos u odrůdy Rulandské modré nesmí překročit 1,5
+        kg hroznů na keř a výnos u odrůdy Tramín nesmí překročit 2 kg hroznů na
+        keř.
+      </p>
+      <p className="mb-3">
+        Hrozny musí být zpracovány bez exogenních enzymů a jiných maceračních
+        přídavných látek. Důležitá je minimalizace síření při výrobě vína. U
+        odrůdy Rulandské modré je povolena maximální hranice do 100 mg/l
+        veškerého SO 2 . U odrůdy Tramín je povolena maximální hranice do 120
+        mg/l veškerého SO 2. Není povolen přídavek kyseliny askorbové.
+      </p>
+      <p className="mb-3">
+        Vína lze vyrábět pouze z hroznů révy vinné odpovídajících kvalitativnímu
+        zařazení: víno s přívlastkem pozdní sběr a víno s přívlastkem výběr z
+        hroznů. Obsah alkoholu ve vínech musí být minimálně 12,5 % obj.. Vína by
+        měla být plná a extraktivní a musí vykazovat typické senzorické
+        vlastnosti dané odrůdy.
+      </p>
+      <p className="mb-3">
+        Vína musí být plněna do skleněných lahví o obsahu 0,75 l nebo 1,5 l. Do
+        degustační komise mohou být přihlášena pouze nalahvovaná vína. Prodej
+        vín je povolen od 1. 5. každého kalendářního roku při splnění
+        minimálních dob zrání vín. Po schválení vína degustační komisí budou
+        láhve označovány kolkem Terroir Čejkovice nalepeným kolem hrdla lahve.
+      </p>
+      <p className="mb-3">
+        Degustace probíhá minimálně jednou ročně. Degustační komise je složena
+        ze členů spolku Čtvrtečníci - sdružení vinařů Čejkovice, z.s. a pracuje
+        v minimálním počtu 11 osob.
+      </p>
+      <p className="mb-3">
+        Degustační komise hodnotí vína body a také o vínech hlasuje. Víno je
+        hodnoceno stobodovým systémem mezinárodní unie enologů, ale hodnotí se
+        pouze body „po pěti“ např.: 85, 90, 95 a 100. Minimální bodové
+        ohodnocení pro zařazení do Terroir Čejkovice je 85 bodů. Pokud je vínu
+        uděleno méně jak 80 bodů musí být napsáno odůvodnění. Dále je o víně
+        hlasováno, přičemž pro zařazení do Terroir Čejkovice je nutný souhlas
+        2/3 hodnotících členů degustační komise.
+      </p>
       <h5 className="mb-4">
         Senzorické vlastnosti a technologické požadavky pro jednotlivé odrůdy:
       </h5>{' '}
@@ -9,69 +62,69 @@ const Modal = (): JSX.Element => {
         Typické rulandové, odrůdové, komplexní s jemným projevem po zrání v
         sudu.
       </h6>{' '}
-      <ul className="mb-5">
-        <li className="mb-3">
-          víno musí mít obsah zbytkového cukru (fruktóza + glukóza) maximálně 4
-          g/l
-        </li>
-        <li className="mb-3">
-          víno musí mít obsah alkoholu minimálně 12,5 % obj.
-        </li>
-        <li className="mb-3">víno musí zrát minimálně 12 měsíců v sudech</li>
-        <li className="mb-3">musí být provedena jablečno-mléčná fermentace</li>
-        <li className="mb-3">
-          víno musí zrát na jemných kalech minimálně 120 dní od konce fermentace
-          nebo 150 dní od sklizně
-        </li>
-        <li className="mb-3">
-          prodej vín je možný nejdříve po 19 měsících od 1. října roku sklizně
-          hroznů
-        </li>
-        <li className="mb-3">
-          při využití nových vypalovaných sudů pro maximálně 33 % celkového
+      <div className="mb-5">
+        <p className="mb-3">
+          Víno musí mít obsah zbytkového cukru (fruktóza + glukóza) maximálně 4
+          g/l.
+        </p>
+        <p className="mb-3">
+          Víno musí mít obsah alkoholu minimálně 12,5 % obj.
+        </p>
+        <p className="mb-3">víno musí zrát minimálně 12 měsíců v sudech.</p>
+        <p className="mb-3">musí být provedena jablečno-mléčná fermentace.</p>
+        <p className="mb-3">
+          Víno musí zrát na jemných kalech minimálně 120 dní od konce fermentace
+          nebo 150 dní od sklizně.
+        </p>
+        <p className="mb-3">
+          Prodej vín je možný nejdříve po 19 měsících od 1. října roku sklizně
+          hroznů.
+        </p>
+        <p className="mb-3">
+          Při využití nových vypalovaných sudů pro maximálně 33 % celkového
           objemu vína, je uvedení vín na trh pod označením Terroir Čejkovice
-          možné nejdříve po 19 měsících od 1. října roku sklizně hroznů
-        </li>
-        <li className="mb-3">
-          při využití nových vypalovaných sudů pro více než 33 % celkového
+          možné nejdříve po 19 měsících od 1. října roku sklizně hroznů.
+        </p>
+        <p className="mb-3">
+          Při využití nových vypalovaných sudů pro více než 33 % celkového
           objemu vína, je uvedení vín na trh pod označením Terroir Čejkovice
-          možné nejdříve po 31 měsících od 1. října roku sklizně hroznů{' '}
-        </li>
-      </ul>
+          možné nejdříve po 31 měsících od 1. října roku sklizně hroznů.
+        </p>
+      </div>
       <h5>TRAMÍN</h5>
       <h6 className="fst-italic mb-4">Hutné, harmonické a kořenité víno</h6>
-      <ul className="mb-5">
-        <li className="mb-3">
-          víno musí mít obsah zbytkového cukru (fruktóza + glukóza) maximálně 4
-          g/l
-        </li>
-        <li className="mb-3">
-          víno musí mít obsah alkoholu minimálně 12,5 % obj.
-        </li>
-        <li className="mb-3">víno musí zrát minimálně 9 měsíců</li>
-        <li className="mb-3">hrozny musí být macerovány minimálně 24 hodin</li>
-        <li className="mb-3">
-          není povolena výroba technologií „oranžových vín“
-        </li>
-        <li className="mb-3">
-          víno musí zrát na jemných kalech minimálně 120 dní od konce fermentace
-          nebo 150 dní od sklizně
-        </li>{' '}
-        <li className="mb-3">
-          prodej vín je možný nejdříve po 19 měsících od 1. října roku sklizně
-          hroznů
-        </li>
-        <li className="mb-3">
-          při využití nových vypalovaných sudů pro maximálně 33 % celkového
+      <div className="mb-5">
+        <p className="mb-3">
+          Víno musí mít obsah zbytkového cukru (fruktóza + glukóza) maximálně 4
+          g/l.
+        </p>
+        <p className="mb-3">
+          Víno musí mít obsah alkoholu minimálně 12,5 % obj.
+        </p>
+        <p className="mb-3">Víno musí zrát minimálně 9 měsíců.</p>
+        <p className="mb-3">Hrozny musí být macerovány minimálně 24 hodin.</p>
+        <p className="mb-3">
+          Není povolena výroba technologií „oranžových vín“.
+        </p>
+        <p className="mb-3">
+          Víno musí zrát na jemných kalech minimálně 120 dní od konce fermentace
+          nebo 150 dní od sklizně.
+        </p>{' '}
+        <p className="mb-3">
+          Prodej vín je možný nejdříve po 19 měsících od 1. října roku sklizně
+          hroznů.
+        </p>
+        <p className="mb-3">
+          Při využití nových vypalovaných sudů pro maximálně 33 % celkového
           objemu vína, je uvedení vín na trh pod označením Terroir Čejkovice
-          možné nejdříve po 19 měsících od 1. října roku sklizně hroznů
-        </li>
-        <li className="mb-3">
-          při využití nových vypalovaných sudů pro více než 33 % celkového
+          možné nejdříve po 19 měsících od 1. října roku sklizně hroznů.
+        </p>
+        <p className="mb-3">
+          Při využití nových vypalovaných sudů pro více než 33 % celkového
           objemu vína, je uvedení vín na trh pod označením Terroir Čejkovice
-          možné nejdříve po 31 měsících od 1. října roku sklizně hroznů
-        </li>
-      </ul>
+          možné nejdříve po 31 měsících od 1. října roku sklizně hroznů.
+        </p>
+      </div>
       <h5> Závěrečné ustanovení </h5> Autor statutu Terroir Čejkovice -
       Čtvrtečníci - sdružení vinařů Čejkovice, z.s. si vyhrazuje právo, jako
       jediný subjekt, upravovat tento statut a to po projednání v pracovní
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -41,17 +41,6 @@ const Home: FC = (): JSX.Element => {
             vína, která obstojí v čase.
           </p>
         </FadeUp>
-        <FadeUp
-          from={{ opacity: 0, translate: '0 2rem' }}
-          to={{ opacity: 1, translate: 'none' }}
-        >
-          <p>
-            Terroirní vína potřebují čas, čas na zrání na sudu a čas na zrání na
-            láhvi. Proto i při jeho vychutnávání nelze spěchat. Dejte těmto
-            vínům vhodnou sklenici a prostor se projevit až do terciálních
-            aromat a zážitek je potom opravdu jedinečný.
-          </p>
-        </FadeUp>
       </div>
 
       <FadeUp
@@ -77,24 +66,18 @@ const Home: FC = (): JSX.Element => {
       </FadeUp>
 
       <div className="container__content light ">
-        <FadeUp from={{ opacity: 0 }} to={{ opacity: 1 }}>
-          <h6 className="fw-bold mb-3">Cíl projektu Terroir Čejkovice je: </h6>
-        </FadeUp>
-
-        <FadeUp from={{ opacity: 0 }} to={{ opacity: 1 }}>
-          <p className="mb-3">Zdůraznění jedinečnosti čejkovických vín.</p>
-        </FadeUp>
-        <FadeUp from={{ opacity: 0 }} to={{ opacity: 1 }}>
-          <p className="mb-3">Zviditelnění odrůd typických pro Čejkovice.</p>
-        </FadeUp>
-        <FadeUp from={{ opacity: 0 }} to={{ opacity: 1 }}>
-          <p className="mb-3">Důraz na vinifikaci a související kvalitu.</p>
-        </FadeUp>
-        <FadeUp from={{ opacity: 0 }} to={{ opacity: 1 }}>
-          <p className="mb-3">
-            Zachování historického odkazu pěstování hroznů v Čejkovicích.
+        <FadeUp
+          from={{ opacity: 0, translate: '0 2rem' }}
+          to={{ opacity: 1, translate: 'none' }}
+        >
+          <p>
+            Terroirní vína potřebují čas, čas na zrání na sudu a čas na zrání na
+            láhvi. Proto i při jeho vychutnávání nelze spěchat. Dejte těmto
+            vínům vhodnou sklenici a prostor se projevit až do terciálních
+            aromat a zážitek je potom opravdu jedinečný.
           </p>
         </FadeUp>
+
         {/* 
         <ScaleIn from={{ scale: '0' }} to={{ scale: '1' }}>
           <Link to="/statut-terroir-cejkovice">
@@ -102,94 +85,6 @@ const Home: FC = (): JSX.Element => {
             <Button className="btn--primary">Více o terroir</Button>
           </Link>
         </ScaleIn> */}
-      </div>
-
-      <div className="container__content">
-        <FadeUp from={{ opacity: 0 }} to={{ opacity: 1 }}>
-          <h2>Statut Terroir Čejkovice</h2>
-        </FadeUp>
-        <FadeUp from={{ opacity: 0 }} to={{ opacity: 1 }}>
-          <p>
-            Označení Terroir Čejkovice je možné přiznávat pouze vínům vyrobených
-            z odrůd:
-          </p>
-        </FadeUp>
-        <ScaleIn from={{ scale: '0' }} to={{ scale: '1' }}>
-          <p className="fw-bold"> Rulandské modré a Tramín.</p>
-        </ScaleIn>
-      </div>
-
-      <div className="container__content light">
-        <ul>
-          <FadeUp from={{ opacity: 0 }} to={{ opacity: 1 }}>
-            <li className="mb-3">
-              {' '}
-              Vína mohou být vyráběna pouze členy sdružení Čtvrtečníci -
-              sdružení vinařů Čejkovice, z.s.
-            </li>
-          </FadeUp>
-          <FadeUp from={{ opacity: 0 }} to={{ opacity: 1 }}>
-            <li className="mb-3">
-              Vína musí být vyráběna pouze z hroznů vypěstovaných na
-              registrovaných vinicích v katastru obce Čejkovice s minimálním
-              počtem 4.500 keřů na hektar. Hrozny musí být zdravé. Víno by mělo
-              být ve větší míře formované vinohradem = terroir. Výnos u odrůdy
-              Rulandské modré nesmí překročit 1,5 kg hroznů na keř a výnos u
-              odrůdy Tramín nesmí překročit 2 kg hroznů na keř.
-            </li>
-          </FadeUp>
-
-          <FadeUp from={{ opacity: 0 }} to={{ opacity: 1 }}>
-            <li className="mb-3">
-              Hrozny musí být zpracovány bez exogenních enzymů a jiných
-              maceračních přídavných látek. Důležitá je minimalizace síření při
-              výrobě vína. U odrůdy Rulandské modré je povolena maximální
-              hranice do 100 mg/l veškerého SO 2 . U odrůdy Tramín je povolena
-              maximální hranice do 120 mg/l veškerého SO 2. Není povolen
-              přídavek kyseliny askorbové.
-            </li>
-          </FadeUp>
-          <FadeUp from={{ opacity: 0 }} to={{ opacity: 1 }}>
-            <li className="mb-3">
-              Vína lze vyrábět pouze z hroznů révy vinné odpovídajících
-              kvalitativnímu zařazení: víno s přívlastkem pozdní sběr a víno s
-              přívlastkem výběr z hroznů. Obsah alkoholu ve vínech musí být
-              minimálně 12,5 % obj.. Vína by měla být plná a extraktivní a musí
-              vykazovat typické senzorické vlastnosti dané odrůdy.
-            </li>
-          </FadeUp>
-
-          <FadeUp from={{ opacity: 0 }} to={{ opacity: 1 }}>
-            <li className="mb-3">
-              Vína musí být plněna do skleněných lahví o obsahu 0,75 l nebo 1,5
-              l. Do degustační komise mohou být přihlášena pouze nalahvovaná
-              vína. Prodej vín je povolen od 1. 5. každého kalendářního roku při
-              splnění minimálních dob zrání vín. Po schválení vína degustační
-              komisí budou láhve označovány kolkem Terroir Čejkovice nalepeným
-              kolem hrdla lahve.
-            </li>
-          </FadeUp>
-          <FadeUp from={{ opacity: 0 }} to={{ opacity: 1 }}>
-            <li className="mb-3">
-              Degustace probíhá minimálně jednou ročně. Degustační komise je
-              složena ze členů spolku Čtvrtečníci - sdružení vinařů Čejkovice,
-              z.s. a pracuje v minimálním počtu 11 osob.
-            </li>
-          </FadeUp>
-
-          <FadeUp from={{ opacity: 0 }} to={{ opacity: 1 }}>
-            <li className="mb-3">
-              Degustační komise hodnotí vína body a také o vínech hlasuje. Víno
-              je hodnoceno stobodovým systémem mezinárodní unie enologů, ale
-              hodnotí se pouze body „po pěti“ např.: 85, 90, 95 a 100. Minimální
-              bodové ohodnocení pro zařazení do Terroir Čejkovice je 85 bodů.
-              Pokud je vínu uděleno méně jak 80 bodů musí být napsáno
-              odůvodnění. Dále je o víně hlasováno, přičemž pro zařazení do
-              Terroir Čejkovice je nutný souhlas 2/3 hodnotících členů
-              degustační komise.
-            </li>
-          </FadeUp>
-        </ul>
 
         <div>
           <ScaleIn from={{ scale: '0' }} to={{ scale: '1' }}>
