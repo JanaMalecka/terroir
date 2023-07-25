e629a78526c28c66f589813e757e26389d1954ca
JanaMalecka <jm.jmalecka@gmail.com>
2023-07-25 11:31:26 +0200

    statute button added into map section

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
 import map from '../assets/images/map.webp';
+import ScaleIn from '../features/Animate/ScaleIn';
+import Modal from '../components/Modal';
 
 const Map: FC = (): JSX.Element => {
   return (
@@ -12,6 +14,46 @@ const Map: FC = (): JSX.Element => {
           className="img-style mb-5"
           loading="lazy"
         />
+      </div>
+      <div className="container__content">
+        <div>
+          <ScaleIn from={{ scale: '0' }} to={{ scale: '1' }}>
+            <button
+              type="button"
+              className="btn--primary"
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal"
+            >
+              Více o terroir
+            </button>
+          </ScaleIn>
+
+          <div
+            className="modal fade"
+            id="exampleModal"
+            /*  tabindex="-1" */
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog modal-dialog-scrollable">
+              <div className="modal-content">
+                <div className="modal-header modal-separator--color d-flex flex-column">
+                  <h2 className="pt-3">Statut TERROIR ČEJKOVICE</h2>
+                </div>
+                <Modal />
+                <div className="modal-footer modal-separator--color">
+                  <button
+                    type="button"
+                    className="btn--secondary"
+                    data-bs-dismiss="modal"
+                  >
+                    Zavřít
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
