838b10d43ad6da2a844a86acb3272b9880c90550
JanaMalecka <jm.jmalecka@gmail.com>
2023-09-24 14:36:30 +0200

    winaries title and subtitles improved

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -8,6 +8,7 @@ interface SlideProps extends StackedCarouselSlideProps {
     id: number;
     image: string;
     title: string;
+    titleLarge: string;
     subtitle: string;
     motto: string;
     text: string[];
@@ -29,6 +30,7 @@ const Slide: React.FC<SlideProps> = React.memo(function (props) {
   const coverImage = data[dataIndex].image;
   const text = data[dataIndex].text;
   const title = data[dataIndex].title;
+  const titleLarge = data[dataIndex].titleLarge;
   const subtitle = data[dataIndex].subtitle;
   const motto = data[dataIndex].motto;
   const web = data[dataIndex].contact.web;
@@ -85,16 +87,56 @@ const Slide: React.FC<SlideProps> = React.memo(function (props) {
             /> */}
             <div
               onClick={handleClick}
-              className={`${isCenterSlide ? 'carousel-subtitle' : 'none'}`}
+              className={`${isCenterSlide ? 'carousel-subtitle' : 'none'} ${
+                id === 10 ||
+                id === 7 ||
+                id === 2 ||
+                id === 8 ||
+                id === 12 ||
+                id === 9 ||
+                id === 11 ||
+                id === 13 ||
+                id === 3 ||
+                id === 1 ||
+                id === 4
+                  ? 'carousel-subtitle--right'
+                  : 'carousel-subtitle'
+              }`}
             >
               {subtitle}
             </div>
             <div
               onClick={handleClick}
-              className={`${isCenterSlide ? 'carousel-title' : 'none'}`}
+              className={`${isCenterSlide ? 'carousel-title' : 'none'} ${
+                isCenterSlide &&
+                (id === 10 ||
+                  id === 7 ||
+                  id === 2 ||
+                  id === 8 ||
+                  id === 9 ||
+                  id === 11 ||
+                  id === 13 ||
+                  id === 3 ||
+                  id === 4 ||
+                  id === 12 ||
+                  id === 1)
+                  ? 'carousel-title--right'
+                  : 'carousel-title'
+              } `}
             >
               {title}
             </div>
+            <div
+              onClick={handleClick}
+              className={`${
+                isCenterSlide && (id === 1 || id === 12 || id === 4)
+                  ? 'carousel-titleLarge'
+                  : 'none'
+              }`}
+            >
+              {titleLarge}
+            </div>
+
             <div className="divider"></div>
 
             <div
