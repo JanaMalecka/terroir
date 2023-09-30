7d18a80e95d2d0ffc94a51dd6be9aad5b9772c75
JanaMalecka <jm.jmalecka@gmail.com>
2023-09-30 07:08:54 +0200

    home colors and link changed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,53 +3,53 @@ import React, { FC } from 'react';
 import vineyardWideLow from '../assets/images/vineyardWideLow.webp';
 import FadeUp from '../features/Animate/FadeUp';
 import ScaleIn from '../features/Animate/ScaleIn';
-import Modal from '../components/Modal';
+import Button from '../components/Button';
 
 const Home: FC = (): JSX.Element => {
   return (
     <>
       <section className="container__content--mobile"></section>
-      <section className="container--dark">
-        <div className="container__content">
-          <h1>TERROIR ČEJKOVICE</h1>
 
-          <p>
-            První apelační systém, který ukazuje neopakovatelnou typičnost odrůd
-            Tramín a Rulandské modré pěstovaných na jedinečných viničních
-            tratích Čejkovic.{' '}
-          </p>
-        </div>
+      <div className="container__content">
+        <h1>TERROIR ČEJKOVICE</h1>
 
-        <div className="container__content">
-          <FadeUp
-            from={{ opacity: 0, translate: '0 2rem' }}
-            to={{ opacity: 1, translate: 'none' }}
-          >
-            <p>
-              Výběr Tramínu a Rulandského modrého vycházel ze staletých
-              zkušeností, kdy vína z těchto odrůd jsou díky klimatickým a půdním
-              podmínkám Čejkovic naprosto specifická a vynikají svou kvalitou a
-              dlouhověkostí.
-            </p>
-          </FadeUp>
-          <FadeUp
-            from={{ opacity: 0, translate: '0 2rem' }}
-            to={{ opacity: 1, translate: 'none' }}
-          >
-            <p className="fw-bold">
-              Výroba vín zařazených do systému TERROIR ČEJKOVICE se řídí přísným
-              statutem pro pěstování a vinifikaci, tak aby mohla vznikat
-              komplexní vína, která obstojí v čase.
-            </p>
-          </FadeUp>
-        </div>
+        <p>
+          První apelační systém, který ukazuje neopakovatelnou typičnost odrůd
+          Tramín a Rulandské modré pěstovaných na jedinečných viničních tratích
+          Čejkovic.{' '}
+        </p>
+      </div>
 
+      <div className="container__content">
         <FadeUp
-          from={{ opacity: 1, translate: '0 2rem' }}
+          from={{ opacity: 0, translate: '0 2rem' }}
           to={{ opacity: 1, translate: 'none' }}
         >
-          <div className="d-flex justify-content-center">
-            {/*   <LazyLoadImage
+          <p>
+            Výběr Tramínu a Rulandského modrého vycházel ze staletých
+            zkušeností, kdy vína z těchto odrůd jsou díky klimatickým a půdním
+            podmínkám Čejkovic naprosto specifická a vynikají svou kvalitou a
+            dlouhověkostí.
+          </p>
+        </FadeUp>
+        <FadeUp
+          from={{ opacity: 0, translate: '0 2rem' }}
+          to={{ opacity: 1, translate: 'none' }}
+        >
+          <p className="fw-bold">
+            Výroba vín zařazených do systému TERROIR ČEJKOVICE se řídí přísným
+            statutem pro pěstování a vinifikaci, tak aby mohla vznikat komplexní
+            vína, která obstojí v čase.
+          </p>
+        </FadeUp>
+      </div>
+
+      <FadeUp
+        from={{ opacity: 1, translate: '0 2rem' }}
+        to={{ opacity: 1, translate: 'none' }}
+      >
+        <div className="d-flex justify-content-center">
+          {/*   <LazyLoadImage
             src={vineyardWideLow}
             className="img-style"
             alt="Foto vinice"
@@ -57,29 +57,29 @@ const Home: FC = (): JSX.Element => {
             delayMethod="debounce"
             visibleByDefault={true}
           /> */}
-            <img
-              alt="Foto vinice"
-              src={vineyardWideLow}
-              className="img-style"
-              loading="lazy"
-            />
-          </div>
+          <img
+            alt="Foto vinice"
+            src={vineyardWideLow}
+            className="img-style"
+            loading="lazy"
+          />
+        </div>
+      </FadeUp>
+
+      <div className="container__content light ">
+        <FadeUp
+          from={{ opacity: 0, translate: '0 2rem' }}
+          to={{ opacity: 1, translate: 'none' }}
+        >
+          <p>
+            Terroirní vína potřebují čas, čas na zrání na sudu a čas na zrání na
+            láhvi. Proto i při jeho vychutnávání nelze spěchat. Dejte těmto
+            vínům vhodnou sklenici a prostor se projevit až do terciálních
+            aromat a zážitek je potom opravdu jedinečný.
+          </p>
         </FadeUp>
 
-        <div className="container__content light ">
-          <FadeUp
-            from={{ opacity: 0, translate: '0 2rem' }}
-            to={{ opacity: 1, translate: 'none' }}
-          >
-            <p>
-              Terroirní vína potřebují čas, čas na zrání na sudu a čas na zrání
-              na láhvi. Proto i při jeho vychutnávání nelze spěchat. Dejte těmto
-              vínům vhodnou sklenici a prostor se projevit až do terciálních
-              aromat a zážitek je potom opravdu jedinečný.
-            </p>
-          </FadeUp>
-
-          {/* 
+        {/* 
         <ScaleIn from={{ scale: '0' }} to={{ scale: '1' }}>
           <Link to="/statut-terroir-cejkovice">
             {' '}
@@ -87,46 +87,20 @@ const Home: FC = (): JSX.Element => {
           </Link>
         </ScaleIn> */}
 
-          <div>
-            <ScaleIn from={{ scale: '0' }} to={{ scale: '1' }}>
-              <button
-                type="button"
-                className="btn--primary"
-                data-bs-toggle="modal"
-                data-bs-target="#exampleModal"
-              >
-                Více o terroir
-              </button>
-            </ScaleIn>
-
-            <div
-              className="modal fade"
-              id="exampleModal"
-              /*  tabindex="-1" */
-              aria-labelledby="exampleModalLabel"
-              aria-hidden="true"
+        <div>
+          <ScaleIn from={{ scale: '0' }} to={{ scale: '1' }}>
+            <a
+              href={`http://localhost:3000/statut-terroir-cejkovice`}
+              className="link-dark"
+              aria-label={`Přejít na webové stránky Statut terroir Čejkovice`}
+              target="_self"
+              rel="noreferrer"
             >
-              <div className="modal-dialog modal-dialog-scrollable">
-                <div className="modal-content">
-                  <div className="modal-header modal-separator--color d-flex flex-column">
-                    <h2 className="pt-3">Statut TERROIR ČEJKOVICE</h2>
-                  </div>
-                  <Modal />
-                  <div className="modal-footer modal-separator--color">
-                    <button
-                      type="button"
-                      className="btn--secondary"
-                      data-bs-dismiss="modal"
-                    >
-                      Zavřít
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+              <Button className="btn--secondary">Více o terroir</Button>
+            </a>
+          </ScaleIn>
         </div>
-      </section>
+      </div>
     </>
   );
 };
