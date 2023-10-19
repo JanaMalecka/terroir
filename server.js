ad5df3a2d4349c3b72534f4b5d3190e9b81b99a6
JanaMalecka <jm.jmalecka@gmail.com>
2023-10-20 00:24:33 +0200

    fix with server app.get

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 5000; // Set the port to your preferred value
 
-// Serve static assets (your React app's build folder)
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Handle requests for all routes by serving the index.html
-app.get('*', (req, res) => {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+app.listen(9000);
