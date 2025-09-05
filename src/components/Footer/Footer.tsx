164668c7c0750d8889f3c10c68a57e80e2ae1bcc
JanaMalecka <jm.jmalecka@gmail.com>
2025-09-05 05:49:02 +0200

    changes reverted; applied updates to wines, and fixed footer styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,58 +4,54 @@ const Footer: FC = (): JSX.Element => {
   return (
     <footer className="container-footer">
       <div className="footer-section">
-        <div>
-          <div className="footer-subsection">
-            <div className="mb-3 d-flex footer-left">
-              <a
-                href="https://www.vinozcejkovic.cz"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="Přejít na webové stránky vinozcejkovic.cz"
-              >
-                <div className="logo-vino-z-cejkovic"></div>
-              </a>
-            </div>
-            <div className="mb-3">
-              <div className="mb-3">
-                <div className="d-flex justify-content-center">
-                  <a
-                    href="https://www.facebook.com/vinozcejkovic"
-                    target="_blank"
-                    rel="noreferrer"
-                    aria-label="Přejít na Facebook spolku Víno z Čejkovic"
-                  >
-                    <div className="icon-facebook"></div>
-                  </a>
-                  <a
-                    href="https://www.instagram.com/vinozcejkovic"
-                    target="_blank"
-                    rel="noreferrer"
-                    aria-label="Přejít na Instagram spolku Víno z Čejkovic"
-                  >
-                    <div className="icon-instagram"></div>
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <div className="mb-3 footer-right">
-              <a
-                href="https://www.terroircejkovice.cz"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="Přejít na webové stránky terroircejkovice.cz"
-              >
-                <div className="logo-terroir--light"></div>
-              </a>
-            </div>
+        <div className="footer-subsection">
+          {/* Left logo */}
+          <div className="footer-left">
+            <a
+              href="https://www.vinozcejkovic.cz"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="Přejít na webové stránky vinozcejkovic.cz"
+            >
+              <div className="logo-vino-z-cejkovic"></div>
+            </a>
           </div>
-          <div>© 2023 - 2024 Čtvrtečníci - sdružení vinařů Čejkovice</div>
-          {/* <div>
-        Používáním těchto webových stránek souhlasíte s použitím souborů
-        cookies.
-      </div> */}
+
+          {/* Social icons */}
+          <div className="d-flex justify-content-center">
+            <a
+              href="https://www.facebook.com/vinozcejkovic"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="Přejít na Facebook spolku Víno z Čejkovic"
+            >
+              <div className="icon-facebook"></div>
+            </a>
+            <a
+              href="https://www.instagram.com/vinozcejkovic"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="Přejít na Instagram spolku Víno z Čejkovic"
+            >
+              <div className="icon-instagram"></div>
+            </a>
+          </div>
+
+          {/* Right logo */}
+          <div className="footer-right">
+            <a
+              href="https://www.terroircejkovice.cz"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="Přejít na webové stránky terroircejkovice.cz"
+            >
+              <div className="logo-terroir--light"></div>
+            </a>
+          </div>
         </div>
+
+        {/* Footer text */}
+        <div>© 2023 - 2025 Čtvrtečníci - sdružení vinařů Čejkovice</div>
       </div>
     </footer>
   );
